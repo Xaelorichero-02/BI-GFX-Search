d48b1d65eb727ec793cd712f9f27111ab4a38263
Xaelor <77115546+Xaelorichero-02@users.noreply.github.com>
2025-07-15 12:55:49 +0200

    focuses

---
+++ gfx_blocks.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Set path to subfolder containing the .png files
+set "targetFolder=gfx\interface\goals"
+
+:: Output file
+set "output=gfx_blocks.txt"
+echo. > "%output%"
+
+:: Loop through .png files in the subfolder
+for %%f in (%targetFolder%\*.png) do (
+    set "filename=%%~nf"
+    echo ^<div data-clipboard-text="GFX_goal_!filename!" data-search-text="GFX_goal_!filename!" title="GFX_goal_!filename!" class="icon dlc-base"^> >> "%output%"
+    echo     ^<img loading="lazy" src="gfx\interface\goals\!filename!.png" alt="GFX_goal_!filename!"^> >> "%output%"
+    echo ^</div^> >> "%output%"
+    echo. >> "%output%"
+)
+
+echo Done. Output written to %output%.
+pause
